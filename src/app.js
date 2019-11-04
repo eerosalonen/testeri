98bd5908a006cdb0712ea9a17cfb56d78a5f3f09
eerosalonen <34651968+eerosalonen@users.noreply.github.com>
2019-11-04 18:24:20 +0200

    vvv
    vvvvvvv

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const app = express();
 const path = require("path");
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+app.use("/static", express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index");
