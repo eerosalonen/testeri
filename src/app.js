7e0f8c0b73d8e69d8f08beca1d376d60b0150222
eerosalonen <34651968+eerosalonen@users.noreply.github.com>
2019-11-04 16:55:30 +0200

    pohjapug
    pug toimii

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 const path = require("path");
-app.set("src", path.join(__dirname, "src"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
