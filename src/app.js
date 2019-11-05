3aac59c5398c43de834b60a0d2376671aaee3203
eerosalonen <34651968+eerosalonen@users.noreply.github.com>
2019-11-05 21:42:14 +0200

    toimii
    joo

--- src/app.js
+++ src/app.js
@@ -2,6 +2,70 @@ const express = require("express");
 
 const app = express();
 const path = require("path");
+var mongoose = require("mongoose");
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+  ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0",
+  mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+  mongoURLLabel = "";
+var port = normalizePort(port || "3000");
+
+if (mongoURL == null) {
+  var mongoHost, mongoPort, mongoDatabase, mongoPassword, mongoUser;
+  // If using plane old env vars via service discovery
+  if (process.env.DATABASE_SERVICE_NAME) {
+    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
+    mongoHost = process.env[mongoServiceName + "_SERVICE_HOST"];
+    mongoPort = process.env[mongoServiceName + "_SERVICE_PORT"];
+    mongoDatabase = process.env[mongoServiceName + "_DATABASE"];
+    mongoPassword = process.env[mongoServiceName + "_PASSWORD"];
+    mongoUser = process.env[mongoServiceName + "_USER"];
+
+    // If using env vars from secret from service binding
+  } else if (process.env.database_name) {
+    mongoDatabase = process.env.database_name;
+    mongoPassword = process.env.password;
+    mongoUser = process.env.username;
+    var mongoUriParts = process.env.uri && process.env.uri.split("//");
+    if (mongoUriParts.length == 2) {
+      mongoUriParts = mongoUriParts[1].split(":");
+      if (mongoUriParts && mongoUriParts.length == 2) {
+        mongoHost = mongoUriParts[0];
+        mongoPort = mongoUriParts[1];
+      }
+    }
+  }
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    mongoURLLabel = mongoURL = "mongodb://";
+    if (mongoUser && mongoPassword) {
+      mongoURL += mongoUser + ":" + mongoPassword + "@";
+    }
+    // Provide UI label that excludes user id and pw
+    mongoURLLabel += mongoHost + ":" + mongoPort + "/" + mongoDatabase;
+    mongoURL += mongoHost + ":" + mongoPort + "/" + mongoDatabase;
+  }
+}
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+// Connecting to DB
+mongoose.connect(mongoURL);
+mongoose.Promise = Promise;
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
